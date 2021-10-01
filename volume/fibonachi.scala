8c057b9b97516a75d11530595a8eba104851c716
SewonYun <facemen9459@gmail.com>
2021-10-02 03:20:55 +0900

    add: fibonachi def

--- volume/fibonachi.scala
+++ volume/fibonachi.scala
@@ -1,3 +1,13 @@
-def fib(n: Int): Int = {
+def fib(n: BigInt): BigInt = {
+
+  def go(nth:BigInt, p2: BigInt, p1: BigInt): BigInt = {
+    if (nth == 1) return p2
+    if (nth == 2) return p1
+
+    go(nth - 1, p1, p1 + p2)
+  }
   
+  go(n, 0, 1)
 }
+val rst = fib(202)
+println(rst)
