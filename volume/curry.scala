3303bcff914db02e97c411a20a51ed3788d35914
SewonYun <facemen9459@gmail.com>
2021-10-19 22:42:50 +0900

    add uncurry in curry.scala

--- volume/curry.scala
+++ volume/curry.scala
@@ -1,4 +1,14 @@
 def curry [A, B, C](f: (A, B) => C) : A => (B => C) = 
   (a: A) => (b: B) => f(a, b)
 
-print(curry((a: Int, b: Int) => a + b)(2)(3))
+val plus = curry((a: Int, b: Int) => a + b)
+val plus12 = plus(12)
+println(plus12(5))
+
+
+def uncurry [A, B, C](f: A => (B => C)) : (A, B) => C = 
+  (a: A, b: B) => f(a)(b)
+
+val pre = uncurry(plus)
+
+println(pre(1, 3))
