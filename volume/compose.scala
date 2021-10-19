c0f28a953fa487c73c45f5bf720ce3c811d4870e
SewonYun <facemen9459@gmail.com>
2021-10-19 23:33:04 +0900

    add trainning quest compose funtion

--- volume/compose.scala
+++ volume/compose.scala
@@ -1,5 +1,13 @@
 def compose[A, B, C](f: B => C, g: A => B): A => C =
   (a: A) => f(g(a))
 
-val doubleTwice = compose((a: Int) => a * a, (b: Int) => b * 2)
-println(doubleTwice(3)) // 36
+val doubleSquare = compose((a: Int) => a * a, (b: Int) => b * 2)
+println(doubleSquare(3)) // 36
+
+val square = (a: Int) => a * a
+val double = (b: Int) => b * 2
+val stdComposed = square compose double // like  f compose g
+val stdAndThen = double andThen square // like  f compose g
+
+println(stdComposed(3)) // 36
+println(stdAndThen(4)) // 64
